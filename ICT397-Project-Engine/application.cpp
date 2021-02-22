cb97fca1b3ea40f473d3bffe44a578d27e0da05a
Bryce Standley <nukeedits787@gmail.com>
2021-02-22 16:21:32 +0800

    0.0.0 - GLFW Test - Basic colour shift of whole window to check if GLFW is working

--- ICT397-Project-Engine/application.cpp
+++ ICT397-Project-Engine/application.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
+/*Test includes and defines to be removed*/
+#include <cmath>
+#include <chrono>
+#include <thread>
+#define PI 3.14159265358
 
 /**
 * Basic setup for defult GLFW Window
@@ -26,11 +31,31 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    GLclampf red = 0.0f;
+    GLclampf green = 0.0f;
+    GLclampf blue = 0.0f;
+    GLclampf alpha = 1.0f;
+    float x = -1.0f;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
+
+        /*Small Background Colour changer for GLFW test*/
+        /*Remove below to clear Test from file*/
+        red = (sin(x) + 1) / 2.0;
+        green = (sin(x+1) + 1) / 2.0;
+        blue = (sin(x+2) + 1) / 2.0;
+        x += 0.1;
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+        glClearColor(red, green, blue, alpha);
+        /*Remove Above to clear test from file*/
+
+
         glClear(GL_COLOR_BUFFER_BIT);
+        
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
