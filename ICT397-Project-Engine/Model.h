2889deac032a83ed0d74aacc8608d486185c6bf7
Dylan <dylanblereau@yahoo.com>
2021-04-05 20:13:03 +0800

    Added Doxy comments for Skybox and Camera

--- ICT397-Project-Engine/Model.h
+++ ICT397-Project-Engine/Model.h
@@ -34,7 +34,7 @@
 class Model
 {
 public:
-    /// model data 
+        /// model data 
     std::vector<TextureMesh> textures_loaded;
     std::vector<Mesh>    meshes;
     Renderer* render;
@@ -45,59 +45,81 @@ public:
     /// default constructor
     Model() {}
 
-    /**
-    * @brief constructor that loads in the model from the given path
-    * @param path - represents the model path
-    * @param gamma - determines whether or not gamma correction is applied
-    */
+        /**
+        * @brief constructor that loads in the model from the given path
+        * @param path - represents the model path
+        * @param r - pointer to a renderer object
+        * @param gamma - determines whether or not gamma correction is applied
+        */
     Model(std::string const& path, Renderer* r, bool gamma = false) : gammaCorrection(gamma)
     {
         render = r;
         loadModel(path);
     }
 
-    /**
-    * @brief draws the model, and thus all it meshes
-    * @param shader - represents the models shader object
-    * @param model - represents the model matrix
-    */
+        /**
+        * @brief draws the model, and thus all it meshes
+        * @param shader - represents the models shader object
+        * @param model - represents the model matrix
+        */
     void Draw(Shader& shader, glm::mat4 model);
 
 
 private:
 
-    /**
-    * @brief converts a aiMatrix4x4t to a glm::mat4
-    * @param m - represents a 4x4 matrix of floats
-    * @return glm::mat4
-    */
+        /**
+        * @brief converts a aiMatrix4x4t to a glm::mat4
+        * @param m - represents a 4x4 matrix of floats
+        * @return glm::mat4
+        */
     glm::mat4 to_glm(aiMatrix4x4t<float> m);
 
-    /**
-    * @brief converts a aiVector3t to a glm::vec3
-    * @param v - represents a vector of 3 floats
-    * @return glm::vec3
-    */
+        /**
+        * @brief converts a aiVector3t to a glm::vec3
+        * @param v - represents a vector of 3 floats
+        * @return glm::vec3
+        */
     glm::vec3 to_glm(aiVector3t<float> v);
 
-    /**
-    * @brief converts a aiQuaterniont to a glm::quat
-    * @param q - represents a quaternion of 4 floats
-    * @return glm::quat
-    */
+        /**
+        * @brief converts a aiQuaterniont to a glm::quat
+        * @param q - represents a quaternion of 4 floats
+        * @return glm::quat
+        */
     glm::quat to_glm(aiQuaterniont<float> q);
 
 
-    /**
-    * @brief loads a model via assimp from file and stores the meshes in a vector
-    * @param path - represents the path of the model
-    */
+        /**
+        * @brief loads a model via assimp from file and stores the meshes in a vector
+        * @param path - represents the path of the model
+        */
     void loadModel(std::string const& path);
 
+        /**
+        * @brief processes a node in a recursive fashion. Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
+        * @param node
+        * @param scene
+        * @param transform
+        */
     void processNode(aiNode* node, const aiScene* scene, glm::mat4 transform);
 
+        /**
+        * @brief translates the aiMesh object to a mesh object by running through the mesh's properties and storing data in the object
+        * @param mesh - represents a pointer to the aiMesh
+        * @param scene - represents a pointer to the scene
+        * @param transform - represents the transform matrix required to properly set up models
+        * @return Mesh
+        */
     Mesh processMesh(aiMesh* mesh, const aiScene* scene, glm::mat4 transform);
 
+        /**
+        *
+        * @brief check all material textures of a given type and loads the texture in if they haven't loaded yet
+        * @param mat - represents assimps aiMaterial type
+        * @param type - represents assimps aiTextureType
+        * @param typeName - represents a string denoting the type of the texture material
+        * @return std::vector<TextureMesh>
+        */
     std::vector<TextureMesh> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
 
 };
--- ICT397-Project-Engine/Skybox.h
+++ ICT397-Project-Engine/Skybox.h
@@ -5,19 +5,67 @@
 #include <vector>
 #include "Shader.h"
 
+
+	/**
+	* @class Skybox
+	* @brief A class that is responsible for creating a setting up a skybox 
+	*
+	* @author Dylan Blereau
+	* @version 01
+	* @date 27/03/2021 Dylan Blereau, Started
+	*
+	**/
 class Skybox
 {
 public:
+
+		/**
+		* @brief default constructor that initialises the object and sets the vertices of the skybox 
+		*/
 	Skybox() { SetSkyVerts(); }
+
+		/**
+		* @brief a constructor that takes in the paths to the images for each face of the skybox, as well as the paths to its vertex and fragment shader
+		* 
+		* @param right - represents the right face of the skybox
+		* @param left - represents the left face of the skybox
+		* @param top - represents the top face of the skybox
+		* @param bottom - represents the bottom face of the skybox
+		* @param front - represents the front face of the skybox
+		* @param back - represents the back face of the skybox
+		* @param vertexShader - represents the skybox's vertex shader
+		* @param fragmentShader - represents the skybox's fragment shader
+		*/
 	Skybox(std::string right, std::string left, std::string top, std::string bottom, std::string front, std::string back, std::string vertexShader, std::string fragmentShader);
+
+		/**
+		* @brief destructor
+		*/
 	~Skybox() { };
+
+		/// shader object
 	Shader skyShader;
 	unsigned int VAO = 0, VBO = 0;
 	unsigned int texture = 0;
+
+		/**
+		* @brief gets and returns all the cubeFaces that make up the skybox
+		* @return std::vector<std::string> 
+		*/
 	std::vector<std::string> GetCubeFaces() { return cubeFaces; }
+
+		/**
+		* @brief gets and returns all the vertices that make up the skybox
+		* @return std::vector<float>
+		*/
 	std::vector<float> GetSkyVerts() { return skyVerts; }
+
 private:
+		/**
+		* @brief sets all vertices required to draw the skybox correctly
+		*/
 	void SetSkyVerts();
+
 	std::vector<std::string> cubeFaces;
 	std::vector<float> skyVerts;
 };
