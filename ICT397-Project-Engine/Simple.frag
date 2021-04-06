bb0cc9ce44eb216f2375df2211d8206ad770711d
Dylan <dylanblereau@yahoo.com>
2021-04-06 11:55:42 +0800

    Cleaned up resources and file structure

--- ICT397-Project-Engine/Simple.frag
+++
@@ -1,8 +0,0 @@
-// Simple.frag
-#version 330
-
-out vec4 outColor;
-
-void main(void) {
-    outColor = vec4(0.5, 0.0, 0.5, 1.0);
-} 
--- ICT397-Project-Engine/res/scripts/terrain.lua
+++ ICT397-Project-Engine/res/scripts/terrain.lua
@@ -1,15 +1,15 @@
 --terrain.lua
-heightmap = "test-heightmap.raw"
+heightmap = "./res/heightmaps/test-heightmap.raw"
 terrainSize = 512
 xScale = 1
 yScale = 3
 zScale = 1
 
-tex1 = "dirtTile.jpg"
-tex2 = "grass.jpg"
-tex3 = "mountainTile.jpg"
-tex4 = "snowTile.jpg"
-detailMap = "detailMap.jpg"
+tex1 = "./res/images/terrain/dirtTile.jpg"
+tex2 = "./res/images/terrain/grassTile.jpg"
+tex3 = "./res/images/terrain/mountainTile.jpg"
+tex4 = "./res/images/terrain/snowTile.jpg"
+detailMap = "./res/images/terrain/detailMap.jpg"
 
 vs = "./res/shader/vertex.glsl"
 fs = "./res/shader/fragment.glsl"
