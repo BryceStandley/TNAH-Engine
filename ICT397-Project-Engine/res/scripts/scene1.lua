77f348a9974b4a4c5e2c217fa71b6e6ececf4510
Dylan <dylanblereau@yahoo.com>
2021-04-06 23:13:58 +0800

    model mess around

--- ICT397-Project-Engine/res/scripts/scene1.lua
+++ ICT397-Project-Engine/res/scripts/scene1.lua
@@ -4,8 +4,9 @@
 print("Scene x script loaded")
 cs:MakeGameObject("player", "", "", "", 0, 0, 0, 0, 0);
 cs:MakeGameObject("token", "./res/models/tokens/fbx/Free_Hit.fbx", "./res/shader/modelV.glsl", "./res/shader/modelF.glsl", 0.005, 50, 1, 50, 10);
-cs:MakeGameObject("token", "./res/models/tokens/fbx/Free_Hit.fbx", "./res/shader/modelV.glsl", "./res/shader/modelF.glsl", 0.005, 30, 1, 50, 10);
-cs:MakeGameObject("token", "./res/models/tokens/fbx/Free_Hit.fbx", "./res/shader/modelV.glsl", "./res/shader/modelF.glsl", 0.005, 30, 1, 30, 10);
+cs:MakeGameObject("token", "./res/models/tokens/fbx/Speed_Up.fbx", "./res/shader/modelV.glsl", "./res/shader/modelF.glsl", 0.005, 30, 1, 50, 10);
+cs:MakeGameObject("token", "./res/models/tokens/fbx/Double_Damage.fbx", "./res/shader/modelV.glsl", "./res/shader/modelF.glsl", 0.005, 30, 1, 30, 10);
 cs:MakeGameObject("static", "./res/models/environment/Yellow_Tree-With-Texture.fbx", "./res/shader/modelV.glsl", "./res/shader/modelF.glsl", 0.005, 20, 1, 50, 10);
 cs:MakeGameObject("static", "./res/models/environment/tree-birch-tex.fbx", "./res/shader/modelV.glsl", "./res/shader/modelF.glsl", 0.005, 30, 0.5, 40, 10);
 cs:MakeGameObject("static", "./res/models/environment/stone-oval-tex.fbx", "./res/shader/modelV.glsl", "./res/shader/modelF.glsl", 0.02, 60, 0.5, 40, 10);
+
