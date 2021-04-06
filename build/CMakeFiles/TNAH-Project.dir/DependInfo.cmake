2647f0415e9864867a03a6e061f874a9d3e7c286
Bryce Standley <bryce@vectorpixel.net>
2021-04-06 13:49:58 +0800

    Clion Cmake Files

---
+++ build/CMakeFiles/TNAH-Project.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Callbacks.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Callbacks.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Enemy.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Enemy.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Engine.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Engine.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/GameAssetFactory.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GameAssetFactory.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/GameManager.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GameManager.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/GameObject.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GameObject.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/GladRenderer.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GladRenderer.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/GladSkybox.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GladSkybox.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/GlewBuffer.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GlewBuffer.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/GlewShader.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GlewShader.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/GlfwWindow.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GlfwWindow.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Input.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Input.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Mesh.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Mesh.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Model.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Model.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Player.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Player.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Renderer.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Renderer.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Scene.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Scene.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Shader.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Shader.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Skybox.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Skybox.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Static.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Static.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Terrain.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Terrain.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Texture.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Texture.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/TextureLoader.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/TextureLoader.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/Window.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Window.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/application.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/application.cpp.o"
+  "/Volumes/GITS/ICT397-Project/ICT397-Project-Engine/stb.cpp" "/Volumes/GITS/ICT397-Project/build/CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/stb.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ICT397-Project-Engine/ICT397-Project-Engine"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TNAH-Project.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Callbacks.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Enemy.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Engine.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GameAssetFactory.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GameManager.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GameObject.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GladRenderer.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GladSkybox.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GlewBuffer.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GlewShader.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/GlfwWindow.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Input.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Mesh.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Model.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Player.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Renderer.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Scene.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Shader.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Skybox.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Static.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Terrain.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Texture.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/TextureLoader.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/Window.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/application.cpp.o"
+  "CMakeFiles/TNAH-Project.dir/ICT397-Project-Engine/stb.cpp.o"
+  "TNAH-Project"
+  "TNAH-Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TNAH-Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
