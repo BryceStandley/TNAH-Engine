66f7e8e6167576290f8bccff09300046ba6a0357
ChrisDavid1999 <51144955+ChrisDavid1999@users.noreply.github.com>
2021-09-05 19:56:10 +0800

    Physics stuff

---
+++ TNAH-Engine/src/TNAH/Physics/PhysicsEvents.cpp
@@ -0,0 +1,22 @@
+﻿#include "tnahpch.h"
+/*#include "PhysicsEvents.h"
+
+
+void tnah::PhysicsEvents::onContact(const CollisionCallback::CallbackData &callbackData)
+{
+	if(callbackData.getNbContactPairs() < 1) return;
+
+	rp3d::CollisionBody* objectOne = callbackData.getContactPair(0).getBody1();
+	rp3d::CollisionBody* objectTwo = callbackData.getContactPair(0).getBody2();
+	auto t1 = reinterpret_cast<entt::entity>(*static_cast<entt::entity*>(objectOne->getUserData()));
+	auto t2 = reinterpret_cast<entt::entity>(*static_cast<entt::entity*>(objectTwo->getUserData()));
+
+	//Check that there is an entity somehow?
+	//Do stuff
+	
+}
+
+void tnah::PhysicsEvents::onTrigger(const rp3d::OverlapCallback::CallbackData &callbackData)
+{
+
+}*/
